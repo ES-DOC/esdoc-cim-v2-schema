3ad76451e964fcb6476fbd9aa1ae9b61ea7edd2a
David Hassell <d.c.hassell@reading.ac.uk>
2019-04-04 18:26:07 +0100

    Bump version number: 2.0.39

--- __init__.py
+++ __init__.py
@@ -26,7 +26,7 @@ from . import time as time_classes
 NAME = 'cim'
 
 # Ontology version.
-VERSION = '2.0.38'
+VERSION = '2.0.39'
 
 # Ontology doc string.
 DOC = 'ESDOC CIM ontology schema - version 2'
