537a0c2921f7293777505eceb4868436713a9a21
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2021-06-16 17:55:11 +0100

    Apply black as configured to all CIM modules

--- cmip_classes.py
+++ cmip_classes.py
@@ -6,24 +6,31 @@
 """
 
 
-
 def cmip_dataset():
     """
     A CMIP dataset
     """
     return {
-        'type': 'class',
-        'base': 'data.dataset',
-        'is_abstract': False,
-        'is_document': True,
-        'pstr': ('{}', ('name',)),
-        'properties': [
+        "type": "class",
+        "base": "data.dataset",
+        "is_abstract": False,
+        "is_document": True,
+        "pstr": ("{}", ("name",)),
+        "properties": [
             # FIXME: Should we specialise the availability online reosurce rather than do this?
-            ('drs_location', 'drs.drs_publication_dataset', '0.N',
-             "DRS identifier of dataset."),
-            ('originating_simulation', 'linked_to(activity.simulation)', '0.1',
-             "Makes a link back to originating activity."),
-        ]
+            (
+                "drs_location",
+                "drs.drs_publication_dataset",
+                "0.N",
+                "DRS identifier of dataset.",
+            ),
+            (
+                "originating_simulation",
+                "linked_to(activity.simulation)",
+                "0.1",
+                "Makes a link back to originating activity.",
+            ),
+        ],
     }
 
 
@@ -34,24 +41,43 @@ def cmip_simulation():
     """
 
     return {
-        'type': 'class',
-        'base': 'activity.simulation',
-        'is_abstract': False,
-        'is_document': True,
-        'pstr': ('({}/{}/{}{}{}{})',('used','ran_for_experiments','realization_index','initialization_index',
-                                     'physics_index','forcing_index')),
-        'properties': [
+        "type": "class",
+        "base": "activity.simulation",
+        "is_abstract": False,
+        "is_document": True,
+        "pstr": (
+            "({}/{}/{}{}{}{})",
+            (
+                "used",
+                "ran_for_experiments",
+                "realization_index",
+                "initialization_index",
+                "physics_index",
+                "forcing_index",
+            ),
+        ),
+        "properties": [
             # Ensemble member attributes
             # FIXME: There is a DRS class for this collection of attributes, join or not?
-            ('realization_index', 'int', '1.1',
-             'realization number, e.g. 5'),
-            ('initialization_index', 'int', '1.1',
-             'Index variant of initialization method, e.g. 1'),
-            ('physics_index', 'int', '1.1',
-             'index for model physics, e.g. 3'),
-            ('forcing_index', 'int', '1.1',
-             'index for variant of forcing, e.g. 2'),
-            ('variant_info', 'str', '0.1',
-             'description of run variant differences, e.g. forcing: black carbon aerosol only'),
-        ]
+            ("realization_index", "int", "1.1", "realization number, e.g. 5"),
+            (
+                "initialization_index",
+                "int",
+                "1.1",
+                "Index variant of initialization method, e.g. 1",
+            ),
+            ("physics_index", "int", "1.1", "index for model physics, e.g. 3"),
+            (
+                "forcing_index",
+                "int",
+                "1.1",
+                "index for variant of forcing, e.g. 2",
+            ),
+            (
+                "variant_info",
+                "str",
+                "0.1",
+                "description of run variant differences, e.g. forcing: black carbon aerosol only",
+            ),
+        ],
     }
--- shared_classes.py
+++ shared_classes.py
@@ -15,53 +15,68 @@ def text_blob():
 
     """
     return {
-        'type': 'class',
-        'base': None,
-        'is_abstract': False,
-        'pstr': ('{}', ('content',)),
-        'properties': [
-            ('content', 'str', '1.1',
-                "Raw content (including markup)."),
-            ('encoding', 'shared.text_blob_encoding', '1.1',
-                "Content encoding.")
-        ]
+        "type": "class",
+        "base": None,
+        "is_abstract": False,
+        "pstr": ("{}", ("content",)),
+        "properties": [
+            ("content", "str", "1.1", "Raw content (including markup)."),
+            (
+                "encoding",
+                "shared.text_blob_encoding",
+                "1.1",
+                "Content encoding.",
+            ),
+        ],
     }
 
 
 def citation():
-    """An academic reference to published work.
-
-    """
+    """An academic reference to published work."""
     return {
-        'type': 'class',
-        'base': None,
-        'is_abstract': False,
-        'is_document': True,
-        'pstr': ('{}', ('title',)),
-        'properties': [
-            ('abstract', 'str', '0.1',
-                "Abstract providing high level reference overview."),
-            ('authors', 'str', '0.N',
-                "The Authors of the work."),
-            ('bibtex', 'str', '0.1',
-                "A BibTeX reference for the work."),
-            ('citation_detail', 'str', '0.1',
-                "A complete citation string as would appear in a bibliography."),
-            ('collective_title', 'str', '0.1',
-                "Citation collective title, i.e. with all authors declared."),
-            ('context', 'str', '0.1',
-                "Brief text description of why this resource is being cited."),
-            ('doi', 'str', '0.1',
-                "Digital Object Identifier, if it exists."),
-            ('title', 'str', '0.1',
-                "The title of the work."),
-            ('type', 'str', '0.1',
-                "Citation type."),
-            ('year', 'int', '0.1',
-                "Year of publication."),
-            ('url', 'shared.online_resource', '0.1',
-                "Location of electronic version.")
-        ]
+        "type": "class",
+        "base": None,
+        "is_abstract": False,
+        "is_document": True,
+        "pstr": ("{}", ("title",)),
+        "properties": [
+            (
+                "abstract",
+                "str",
+                "0.1",
+                "Abstract providing high level reference overview.",
+            ),
+            ("authors", "str", "0.N", "The Authors of the work."),
+            ("bibtex", "str", "0.1", "A BibTeX reference for the work."),
+            (
+                "citation_detail",
+                "str",
+                "0.1",
+                "A complete citation string as would appear in a bibliography.",
+            ),
+            (
+                "collective_title",
+                "str",
+                "0.1",
+                "Citation collective title, i.e. with all authors declared.",
+            ),
+            (
+                "context",
+                "str",
+                "0.1",
+                "Brief text description of why this resource is being cited.",
+            ),
+            ("doi", "str", "0.1", "Digital Object Identifier, if it exists."),
+            ("title", "str", "0.1", "The title of the work."),
+            ("type", "str", "0.1", "Citation type."),
+            ("year", "int", "0.1", "Year of publication."),
+            (
+                "url",
+                "shared.online_resource",
+                "0.1",
+                "Location of electronic version.",
+            ),
+        ],
     }
 
 
@@ -72,20 +87,16 @@ def extra_attribute():
 
     """
     return {
-        'type': 'class',
-        'base': None,
-        'is_abstract': False,
-        'pstr': ('{}:{}', ('key', 'value')),
-        'properties': [
-            ('doc', 'str', '0.1',
-                "Documentation associated with this key."),
-            ('key', 'str', '1.1',
-                "Name of attribute."),
-            ('type', 'str', '0.1',
-                "If a non-string type, provide type."),
-            ('value', 'str', '1.1',
-                "Value associated with key.")
-        ]
+        "type": "class",
+        "base": None,
+        "is_abstract": False,
+        "pstr": ("{}:{}", ("key", "value")),
+        "properties": [
+            ("doc", "str", "0.1", "Documentation associated with this key."),
+            ("key", "str", "1.1", "Name of attribute."),
+            ("type", "str", "0.1", "If a non-string type, provide type."),
+            ("value", "str", "1.1", "Value associated with key."),
+        ],
     }
 
 
@@ -95,15 +106,21 @@ def nil_reason():
 
     """
     return {
-        'type': 'enum',
-        'is_open': False,
-        'members': [
+        "type": "enum",
+        "is_open": False,
+        "members": [
             ("nil:inapplicable", "There is no value"),
-            ("nil:missing", "The correct value is not available. Furthermore, a correct value may not exist"),
+            (
+                "nil:missing",
+                "The correct value is not available. Furthermore, a correct value may not exist",
+            ),
             ("nil:template", "The value will be available later"),
-            ("nil:unknown", "The correct value is not known at this time. However, a correct value probably exists"),
-            ("nil:withheld", "The value is not divulged")
-        ]
+            (
+                "nil:unknown",
+                "The correct value is not known at this time. However, a correct value probably exists",
+            ),
+            ("nil:withheld", "The value is not divulged"),
+        ],
     }
 
 
@@ -113,20 +130,21 @@ def online_resource():
 
     """
     return {
-        'type': 'class',
-        'base': None,
-        'is_abstract': False,
-        'pstr': ('{}', ('linkage',)),
-        'properties': [
-            ('description', 'str', '0.1',
-                "Detail of how to access the resource."),
-            ('linkage', 'str', '1.1',
-                "A URL."),
-            ('name', 'str', '1.1',
-                "Name of online resource."),
-            ('protocol', 'str', '0.1',
-                "Protocol to use at the linkage.")
-        ]
+        "type": "class",
+        "base": None,
+        "is_abstract": False,
+        "pstr": ("{}", ("linkage",)),
+        "properties": [
+            (
+                "description",
+                "str",
+                "0.1",
+                "Detail of how to access the resource.",
+            ),
+            ("linkage", "str", "1.1", "A URL."),
+            ("name", "str", "1.1", "Name of online resource."),
+            ("protocol", "str", "0.1", "Protocol to use at the linkage."),
+        ],
     }
 
 
@@ -138,25 +156,29 @@ def party():
 
     """
     return {
-        'type': 'class',
-        'base': None,
-        'is_abstract': False,
-        'pstr': ('{}', ('name',)),
-        'is_document': True,
-        'properties': [
-            ('address', 'str', '0.1',
-                "Institutional address."),
-            ('email', 'str', '0.1',
-                "Email address."),
-            ('name', 'str', '0.1',
-                "Name of person or organisation."),
-            ('orcid_id', 'str', '0.1',
-                "Orcid ID if available."),
-            ('is_organisation', 'bool', '0.1',
-                "True if an organisation not a person."),
-            ('url', 'shared.online_resource', '0.1',
-                "URL of person or institution.")
-        ]
+        "type": "class",
+        "base": None,
+        "is_abstract": False,
+        "pstr": ("{}", ("name",)),
+        "is_document": True,
+        "properties": [
+            ("address", "str", "0.1", "Institutional address."),
+            ("email", "str", "0.1", "Email address."),
+            ("name", "str", "0.1", "Name of person or organisation."),
+            ("orcid_id", "str", "0.1", "Orcid ID if available."),
+            (
+                "is_organisation",
+                "bool",
+                "0.1",
+                "True if an organisation not a person.",
+            ),
+            (
+                "url",
+                "shared.online_resource",
+                "0.1",
+                "URL of person or institution.",
+            ),
+        ],
     }
 
 
@@ -167,39 +189,61 @@ def quality_review():
     A future version of this ontology may rename this class.
     """
     return {
-        'type': 'class',
-        'base': None,
-        'is_abstract': False,
-        'is_document': True,
-        'pstr': ('{}: {}', ('target_document', 'quality_status',)),
-        'properties': [
-            ('date', 'str', '1.1',
-                "Date upon which review was made."),
-            ('metadata_reviewer', 'linked_to(shared.party)', '1.1',
-                "Party who made the metadata quality assessment."),
-            ('quality_description', 'str', '1.1',
-                "Assessment of quality of target document."),
-            ('quality_status', 'shared.quality_status', '0.1',
-                "Status from a controlled vocabulary."),
-            ('target_document', 'shared.doc_reference', '1.1',
-                "This is the document about which quality is asserted.")
-        ]
+        "type": "class",
+        "base": None,
+        "is_abstract": False,
+        "is_document": True,
+        "pstr": (
+            "{}: {}",
+            (
+                "target_document",
+                "quality_status",
+            ),
+        ),
+        "properties": [
+            ("date", "str", "1.1", "Date upon which review was made."),
+            (
+                "metadata_reviewer",
+                "linked_to(shared.party)",
+                "1.1",
+                "Party who made the metadata quality assessment.",
+            ),
+            (
+                "quality_description",
+                "str",
+                "1.1",
+                "Assessment of quality of target document.",
+            ),
+            (
+                "quality_status",
+                "shared.quality_status",
+                "0.1",
+                "Status from a controlled vocabulary.",
+            ),
+            (
+                "target_document",
+                "shared.doc_reference",
+                "1.1",
+                "This is the document about which quality is asserted.",
+            ),
+        ],
     }
 
 
 def quality_status():
-    """Assertion as to the review status of document.
-
-    """
+    """Assertion as to the review status of document."""
     return {
-        'type': 'enum',
-        'is_open': False,
-        'members': [
+        "type": "enum",
+        "is_open": False,
+        "members": [
             ("incomplete", "Currently being worked on"),
             ("finalised", "Author has completed document, prior to review"),
             ("under_review", "Document is being reviewed"),
-            ("reviewed", "Document has been formally reviewed and assessed as complete and accurate")
-        ]
+            (
+                "reviewed",
+                "Document has been formally reviewed and assessed as complete and accurate",
+            ),
+        ],
     }
 
 
@@ -209,18 +253,30 @@ def responsibility():
 
     """
     return {
-        'type': 'class',
-        'base': None,
-        'is_abstract': False,
-        'pstr': ('{}:{}', ('role', 'parties')),
-        'properties': [
-            ('parties', 'linked_to(shared.party)', '1.N',
-                "Parties delivering responsibility."),
-            ('role', 'shared.role_code', '1.1',
-                "Role that the party plays or played."),
-            ('when', 'time.time_period', '0.1',
-                "Period when role was active, if no longer.")
-        ]
+        "type": "class",
+        "base": None,
+        "is_abstract": False,
+        "pstr": ("{}:{}", ("role", "parties")),
+        "properties": [
+            (
+                "parties",
+                "linked_to(shared.party)",
+                "1.N",
+                "Parties delivering responsibility.",
+            ),
+            (
+                "role",
+                "shared.role_code",
+                "1.1",
+                "Role that the party plays or played.",
+            ),
+            (
+                "when",
+                "time.time_period",
+                "0.1",
+                "Period when role was active, if no longer.",
+            ),
+        ],
     }
 
 
@@ -229,25 +285,46 @@ def role_code():
     This is an extension and modification of CI_RoleCode from ISO19115.
     """
     return {
-        'type': 'enum',
-        'is_open': False,
-        'members': [
-            ("Principal Investigator", "Key party responsible for the existence of the resource"),
-            ("originator", "Original source for the resource if obtained from elsewhere"),
+        "type": "enum",
+        "is_open": False,
+        "members": [
+            (
+                "Principal Investigator",
+                "Key party responsible for the existence of the resource",
+            ),
+            (
+                "originator",
+                "Original source for the resource if obtained from elsewhere",
+            ),
             ("author", "Party who created (or co-created) resource"),
             ("collaborator", "Contributor to the production of the resource"),
             ("publisher", "Party who published the resource"),
             ("owner", "Party with legal ownership of the resource"),
-            ("processor", "Party who has taken part in the workflow that resulted in this resource"),
+            (
+                "processor",
+                "Party who has taken part in the workflow that resulted in this resource",
+            ),
             ("distributor", "Party who distributes the resource"),
-            ("sponsor", "Party who has invested in the production of the resource"),
+            (
+                "sponsor",
+                "Party who has invested in the production of the resource",
+            ),
             ("user", "Party who uses the resource"),
-            ("point of contact", "Party who can be contacted for acquiring knowledge about or acquisition of the resource"),
+            (
+                "point of contact",
+                "Party who can be contacted for acquiring knowledge about or acquisition of the resource",
+            ),
             ("resource provider", "Party that supplies the resource"),
-            ("custodian", "Party that accepts accountability and responsibility for the source resource"),
-            ("metadata_reviewer", "Party who carried out an independent review of (this) documentation"),
-            ("metadata_author", "Party who created (this) documentation")
-        ]
+            (
+                "custodian",
+                "Party that accepts accountability and responsibility for the source resource",
+            ),
+            (
+                "metadata_reviewer",
+                "Party who carried out an independent review of (this) documentation",
+            ),
+            ("metadata_author", "Party who created (this) documentation"),
+        ],
     }
 
 
@@ -257,33 +334,35 @@ def text_blob_encoding():
 
     """
     return {
-        'type': 'enum',
-        'is_open': False,
-        'members': [
-            ("ascii", "Normal plain text")
-        ]
+        "type": "enum",
+        "is_open": False,
+        "members": [("ascii", "Normal plain text")],
     }
 
 
 def numeric():
-    """ 
+    """
     A number which comes with a unit, potentially from a controlled vocabulary of units.
     #FIXME: Need to work on the relationship between unit_source and base_unit.
     """
     return {
-        'type':'class',
-        'is_abstract':False,
-        'is_document':False,
-        'pstr': ('{}{}', ('value', 'units')),
-        'properties': [
-            ('value', 'float', '1.1', 'Numerical value of number'),
-            ('units', 'str', '1.1', 'Associated Units'),
-            ('unit_enumeration', 'str', '0.1', 'Internal CIM vocabulary'),
-            ('unit_source', 'shared.online_resource', '0.1', 'External vocabulary source'),
-            ('base_unit', 'str', '0.1', 'type of unit in external vocabulary')
+        "type": "class",
+        "is_abstract": False,
+        "is_document": False,
+        "pstr": ("{}{}", ("value", "units")),
+        "properties": [
+            ("value", "float", "1.1", "Numerical value of number"),
+            ("units", "str", "1.1", "Associated Units"),
+            ("unit_enumeration", "str", "0.1", "Internal CIM vocabulary"),
+            (
+                "unit_source",
+                "shared.online_resource",
+                "0.1",
+                "External vocabulary source",
+            ),
+            ("base_unit", "str", "0.1", "type of unit in external vocabulary"),
         ],
-        'constraints': [
-             ('mutually_exclusive', ('unit_enumeration', 'unit_source'), '0.1'),
-        ]      
+        "constraints": [
+            ("mutually_exclusive", ("unit_enumeration", "unit_source"), "0.1"),
+        ],
     }
-
